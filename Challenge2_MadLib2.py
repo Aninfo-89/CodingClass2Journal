bc0c6852dd376f41d35189904f7194bc41b1f607
vivaan-hub <56731435+vivaan-hub@users.noreply.github.com>
2019-11-03 19:38:47 -0800

    Create Challenge2_MadLib2.py

---
+++ Challenge2_MadLib2.py
@@ -0,0 +1,8 @@
+adjective = input('Please enter an adjective: ')
+noun = input('Please enter a noun: ')
+verb = input('Please enter a verb ending in -ed: ')
+name = input('Please enter a name: ')
+print('Your MadLib:')
+print('The', adjective, noun, verb, 'over the lazy brown', name + '.')
+input()
+
