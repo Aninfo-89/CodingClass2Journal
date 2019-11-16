22932444e2f075d2cb4cf3878b71218c4052d8a6
vivaan-hub <56731435+vivaan-hub@users.noreply.github.com>
2019-11-16 10:36:45 -0800

    Delete Challenge1_MadLib.py

--- Challenge1_MadLib.py
+++
@@ -1,8 +0,0 @@
-# MadLib.py
-adjective = input("Please enter an adjective: ")
-name = input("Please enter your name: ")
-verb = input("Please enter a verb: ")
-print("Your MadLib:")
-print("The", adjective, name, verb, "over the lazy brown dog.")
-input()
-
